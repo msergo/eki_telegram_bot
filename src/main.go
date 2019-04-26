26422216ae5aa96a855a8f01835b55712d0d2928
Sergo <venyae2@gmail.com>
2019-04-26 17:26:08 +0300

    add html config, buttons

--- src/main.go
+++ src/main.go
@@ -18,6 +18,7 @@ import (
 //func addButton(text string, dataIndex string) tgbotapi.InlineKeyboardButton {
 //	return tgbotapi.NewInlineKeyboardButtonData(text, dataIndex)
 //}
+var articleStore []string
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
@@ -43,20 +44,39 @@ func main() {
 	updates := bot.ListenForWebhook("/" + os.Getenv("UUID_TOKEN"))
 	go http.ListenAndServe("0.0.0.0:"+os.Getenv("PORT"), nil)
 
+	var buttons []tgbotapi.InlineKeyboardButton
+
 	for update := range updates {
 		if update.Message == nil {
-			ed := tgbotapi.NewEditMessageText(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID, update.CallbackQuery.Data)
-			if _, err := bot.Send(ed); err != nil {
-				log.Panic(err)
+			articleIndex, _ := strconv.ParseInt(update.CallbackQuery.Data, 10, 64)
+			conf := &tgbotapi.EditMessageTextConfig{}
+			conf.ParseMode = "html"
+			conf.MessageID = update.CallbackQuery.Message.MessageID
+			conf.ChatID = update.CallbackQuery.Message.Chat.ID
+			conf.Text = articleStore[articleIndex]
+			//editedTextMsg := tgbotapi.NewEditMessageText(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID, articleStore[articleIndex])
+			buttons = buttons[:0]
+			for i := 0; i < len(articleStore); i++ {
+				but := tgbotapi.NewInlineKeyboardButtonData(strconv.Itoa(i), strconv.Itoa(i))
+				buttons = append(buttons, but)
 			}
+			kbMarkup := tgbotapi.NewInlineKeyboardMarkup(buttons)
+			conf.ReplyMarkup = &kbMarkup
+			//editedKbd := tgbotapi.NewEditMessageReplyMarkup(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID, kbMarkup)
+			if _, err := bot.Send(conf); err != nil {
+				log.Print(err)
+			}
+			callbackConfig := tgbotapi.NewCallback(update.CallbackQuery.ID, "done")
+			bot.AnswerCallbackQuery(callbackConfig)
 			continue
 		}
 
 		articles := translation_fetcher.GetArticles(update.Message.Text)
+		articleStore = articles
+		buttons = buttons[:0]
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, articles[0])
-		var buttons []tgbotapi.InlineKeyboardButton
 		for i := 0; i < len(articles); i++ {
-			but := tgbotapi.NewInlineKeyboardButtonData(strconv.Itoa(i), strconv.Itoa(i)	)
+			but := tgbotapi.NewInlineKeyboardButtonData(strconv.Itoa(i), strconv.Itoa(i))
 			buttons = append(buttons, but)
 		}
 		msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(buttons)
