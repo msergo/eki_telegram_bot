f6ee394ae5d7f791dcf5dcd0813908a734f28a93
sergo <venyae2@gmail.com>
2020-02-29 12:41:13 +0200

    testing

---
+++ src/eki_ee.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"strings"
+)
+
+type EkiEe struct {
+	redisWorker RedisWorker
+	telegram    *tgbotapi.BotAPI
+}
+
+func (e *EkiEe) Init() {
+	var err error
+	redisWorker := InitRedisWorker()
+	_, err = redisWorker.Ping() //TODO: think about Redis failure
+	captureFatalErrorIfNotNull(err)
+	e.redisWorker = redisWorker
+	bot, err := tgbotapi.NewBotAPI(environment.BotToken)
+	captureFatalErrorIfNotNull(err)
+	e.telegram = bot
+	e.telegram.Debug = false
+}
+
+func (e *EkiEe) MakeNewSearchResponse(update tgbotapi.Update) tgbotapi.Chattable {
+	var articles []string
+	searchWord := strings.ToLower(update.Message.Text)
+	articles = e.redisWorker.GetAllArticles(searchWord)
+	if len(articles) == 0 {
+		articles = FetchArticles(searchWord)
+	}
+	if len(articles) == 0 {
+		return nil
+	}
+	e.redisWorker.StoreArticlesSet(searchWord, articles)
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, articles[0])
+	msg.ParseMode = "html"
+	if len(articles) > 1 {
+		msg.ReplyMarkup = MakeReplyMarkup(searchWord, len(articles), "0")
+	}
+	return msg
+}
+
+func (e *EkiEe) MakeArticleSwitchResponse(update tgbotapi.Update) tgbotapi.Chattable {
+	keysArr := strings.Split(update.CallbackQuery.Data, ",") // TODO: refactor here
+	keyword := strings.ToLower(keysArr[0])
+	buttonsLen := e.redisWorker.GetArticlesLenByKeyword(keyword)
+
+	newText := e.redisWorker.GetArticleByIndex(keyword, keysArr[1])
+	dataToSend := tgbotapi.NewEditMessageText(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID, newText)
+	if buttonsLen > 1 {
+		replyMarkup := MakeReplyMarkup(keyword, buttonsLen, keysArr[1])
+		dataToSend.ReplyMarkup = &replyMarkup
+	}
+
+	return dataToSend
+}
--- src/main.go
+++ src/main.go
@@ -6,30 +6,15 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 
 	"github.com/Netflix/go-env"
 	"github.com/getsentry/sentry-go"
-	"github.com/go-telegram-bot-api/telegram-bot-api"
-	"strings"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 var environment Environment
-
-type EkiEe struct {
-	redisWorker RedisWorker
-	bot         *tgbotapi.BotAPI
-}
-
-func (s *EkiEe) Init() {
-	var err error
-	redisWorker := InitRedisWorker()
-	_, err = redisWorker.Ping() //TODO: think about Redis failure
-	captureFatalErrorIfNotNull(err)
-	s.redisWorker = redisWorker
-}
 
 func main() {
 	sentryInitError := sentry.Init(sentry.ClientOptions{
@@ -44,78 +29,41 @@ func main() {
 	capturePanicErrorIfNotNull(err)
 	environment.Extras = es
 
-	redis := InitRedisWorker()
-	_, err = redis.Ping() //TODO: think about Redis failure
-	captureFatalErrorIfNotNull(err)
-	bot, err := tgbotapi.NewBotAPI(environment.BotToken)
-	captureFatalErrorIfNotNull(err)
 	ekiEe := EkiEe{}
 	ekiEe.Init()
 
-	bot.Debug = false
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	log.Printf("Authorized on account %s", ekiEe.telegram.Self.UserName)
 	if environment.Env != "dev" {
-		_, err = bot.SetWebhook(tgbotapi.NewWebhook(environment.WebhookAddress))
+		_, err = ekiEe.telegram.SetWebhook(tgbotapi.NewWebhook(environment.WebhookAddress))
 		capturePanicErrorIfNotNull(err)
-		info, err := bot.GetWebhookInfo()
+		info, err := ekiEe.telegram.GetWebhookInfo()
 		capturePanicErrorIfNotNull(err)
 		if info.LastErrorDate != 0 {
-			capturePanicErrorIfNotNull(errors.New(fmt.Sprintf("Telegram callback failed: %s", info.LastErrorMessage)))
+			capturePanicErrorIfNotNull(fmt.Errorf("Telegram callback failed: %s", info.LastErrorMessage))
 		}
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		bytes, _ := ioutil.ReadAll(r.Body)
-
+	http.HandleFunc("/"+environment.UuidToken, func(w http.ResponseWriter, r *http.Request) {
 		var update tgbotapi.Update
+		var response tgbotapi.Chattable
+
+		bytes, _ := ioutil.ReadAll(r.Body)
 		LogObject(update)
 		json.Unmarshal(bytes, &update)
-		var response tgbotapi.Chattable
+
 		if !IsCallbackQuery(update) {
 			response = ekiEe.MakeNewSearchResponse(update)
+		} else {
+			response = ekiEe.MakeArticleSwitchResponse(update)
 		}
-		response = ekiEe.MakeArticleSwitchResponse(update)
+
 		if response != nil {
-			_, err = bot.Send(response)
+			_, err = ekiEe.telegram.Send(response)
 			captureFatalErrorIfNotNull(err)
 		}
 
 	})
 
-	go http.ListenAndServe("0.0.0.0:"+environment.AppPort, nil)
+	http.ListenAndServe("0.0.0.0:"+environment.AppPort, nil)
 
-}
-func (e *EkiEe) MakeNewSearchResponse(update tgbotapi.Update) tgbotapi.Chattable {
-	var articles []string
-	searchWord := strings.ToLower(update.Message.Text)
-	articles = e.redisWorker.GetAllArticles(searchWord)
-	if len(articles) == 0 {
-		articles = FetchArticles(searchWord)
-	}
-	if len(articles) == 0 {
-		return nil
-	}
-	e.redisWorker.StoreArticlesSet(searchWord, articles)
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, articles[0])
-	msg.ParseMode = "html"
-	if len(articles) > 1 {
-		msg.ReplyMarkup = MakeReplyMarkup(searchWord, len(articles), "0")
-	}
-	return msg
-}
-
-func (e *EkiEe) MakeArticleSwitchResponse(update tgbotapi.Update) tgbotapi.Chattable {
-	keysArr := strings.Split(update.CallbackQuery.Data, ",") // TODO: refactor here
-	keyword := strings.ToLower(keysArr[0])
-	buttonsLen := e.redisWorker.GetArticlesLenByKeyword(keyword)
-
-	newText := e.redisWorker.GetArticleByIndex(keyword, keysArr[1])
-	dataToSend := tgbotapi.NewEditMessageText(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID, newText)
-	if buttonsLen > 1 {
-		replyMarkup := MakeReplyMarkup(keyword, buttonsLen, keysArr[1])
-		dataToSend.ReplyMarkup = &replyMarkup
-	}
-
-	return dataToSend
 }
