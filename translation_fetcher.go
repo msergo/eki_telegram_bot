a8713d87b665d97abc5f15c157caee8950b42293
msergo <22394810+msergo@users.noreply.github.com>
2024-06-29 01:40:26 +0300

    Ukrainian dictionary

--- translation_fetcher.go
+++ translation_fetcher.go
@@ -19,12 +19,12 @@ const (
 	baseURLEstUkr = "http://www.eki.ee/dict/ukraina/index.cgi?Q="
 
 	cartSelector              = ".tervikart"
-	articleUseCaseSelector    = ".m.x_m.m"
+	articleUseCaseSelectorEst = ".m.x_m.m"
 	articleUseCaseSelectorRus = ".ms.leitud_id"
-	translationSelector       = ".x_x[lang=\"ru\"]" 
+	translationSelectorEstRus = ".x_x[lang=\"ru\"]"
 	exampleEstSelector        = ".x_n[lang=\"et\"]"
 	exampleRusSelector        = ".x_qn[lang=\"ru\"]"
-	grammarFormSelector       = ".mv.x_mv.mv[lang=\"et\"]"
+	grammarFormSelectorEst    = ".mv.x_mv.mv[lang=\"et\"]"
 
 	articleUseCaseSelectorEstUkr = ".k_m.m"
 	translationSelectorEstUkr    = ".k_x.x[lang=\"uk\"]"
@@ -46,69 +46,53 @@ func IsMatchingArticle(searchWord string, givenWord string) bool {
 	return isMatch
 }
 
-// TODO: refactor 
-func GetSingleArticleUkr(searchWord string, node *html.Node) (string, bool) {
-	doc := goquery.NewDocumentFromNode(node)
-	var useCase string
-	if isCyrillicScript(searchWord) { // TODO: refactor
-		text := doc.Text()
-		text = strings.Replace(text, ";", "\r\n", -1)
-		useCase = doc.Find(articleUseCaseSelectorRus).Text()
-		if !IsMatchingArticle(searchWord, useCase) {
-			return "", false
-		}
-
-		return text, false
-	}
-	useCase = doc.Find(articleUseCaseSelectorEstUkr).Text()
-	grammarForms := doc.Find(grammarFormSelectorEstUkr).Text()
-	//filter garbage
-	if !IsMatchingArticle(searchWord, useCase) && !IsMatchingArticle(searchWord, grammarForms) {
-		return "", false
-	}
+func extractTranslationsFromDoc(doc *goquery.Document, searchWord string, useCase string, translationSelector string, grammarFormSelector string) []string {
 	var translations []string
-	doc.Find(translationSelectorEstUkr).Each(func(i int, translation *goquery.Selection) {
-		translations = append(translations, translation.Text())
-	})
-
-	if grammarForms == "" {
-		return fmt.Sprintf("<b>%s</b>\r\n%s",
-			useCase,
-			strings.Join(translations, "\r\n"),
-		), false
-	}
-	return fmt.Sprintf("<b>%s</b><i> (%s) </i>\r\n%s",
-			useCase,
-			grammarForms,
-			strings.Join(translations, "\r\n"),
-		),
-		true
-
-}
-// GetSingleArticle get preformatted translation
-func GetSingleArticle(searchWord string, node *html.Node) (string, bool) {
-	doc := goquery.NewDocumentFromNode(node)
-	var useCase string
-	if isCyrillicScript(searchWord) { // TODO: refactor
-		text := doc.Text()
-		text = strings.Replace(text, ";", "\r\n", -1)
-		useCase = doc.Find(articleUseCaseSelectorRus).Text()
-		if !IsMatchingArticle(searchWord, useCase) {
-			return "", false
-		}
-
-		return text, false
-	}
-	useCase = doc.Find(articleUseCaseSelector).Text()
 	grammarForms := doc.Find(grammarFormSelector).Text()
-	//filter garbage
+	// filter garbage
 	if !IsMatchingArticle(searchWord, useCase) && !IsMatchingArticle(searchWord, grammarForms) {
-		return "", false
+		return translations
 	}
-	var translations []string
 	doc.Find(translationSelector).Each(func(i int, translation *goquery.Selection) {
 		translations = append(translations, translation.Text())
 	})
+	return translations
+}
+
+func GetSingleArticleWithDirection(searchWord string, node *html.Node, direction string) (string, bool) {
+	var articeUseCaseSelector string
+	var grammarFormSelector string
+	var translationSelector string
+	var useCase string
+
+	doc := goquery.NewDocumentFromNode(node)
+
+	// TODO: add constants for directions
+	if direction == "est-ukr" {
+		articeUseCaseSelector = articleUseCaseSelectorEstUkr
+		grammarFormSelector = grammarFormSelectorEstUkr
+		translationSelector = translationSelectorEstUkr
+	} else if direction == "est-rus" {
+		articeUseCaseSelector = articleUseCaseSelectorEst
+		grammarFormSelector = grammarFormSelectorEst
+		translationSelector = translationSelectorEstRus
+	} else if direction == "rus-est" {
+		articeUseCaseSelector = articleUseCaseSelectorRus
+
+		text := doc.Text()
+		text = strings.Replace(text, ";", "\r\n", -1)
+		useCase = doc.Find(articeUseCaseSelector).Text()
+
+		if !IsMatchingArticle(searchWord, useCase) {
+			return "", false
+		}
+
+		return text, false
+	}
+
+	useCase = doc.Find(articeUseCaseSelector).Text()
+	grammarForms := doc.Find(grammarFormSelector).Text()
+	translations := extractTranslationsFromDoc(doc, searchWord, useCase, translationSelector, grammarFormSelector)
 
 	if grammarForms == "" {
 		return fmt.Sprintf("<b>%s</b>\r\n%s",
@@ -116,50 +100,66 @@ func GetSingleArticle(searchWord string, node *html.Node) (string, bool) {
 			strings.Join(translations, "\r\n"),
 		), false
 	}
+
 	return fmt.Sprintf("<b>%s</b><i> (%s) </i>\r\n%s",
 			useCase,
 			grammarForms,
 			strings.Join(translations, "\r\n"),
 		),
 		true
-
 }
 
 // GetArticles fetches HTML page and extract separate word-related articles
 func GetArticles(searchWord string) []string {
 	// Request the HTML page.
-	var url string
+	var urls []string
+	var translationDirections []string
 
 	if isCyrillicScript(searchWord) {
-		url = baseURLRusEst
+		urls = []string{baseURLRusEst}
+		translationDirections = []string{"rus-est"}
 	} else {
-		url = baseURLEstRus 
-	}
-	res, err := http.Get(fmt.Sprintf("%s%s", url, searchWord))
-	captureErrorIfNotNull(err)
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		sentry.CaptureException(fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status))
+		urls = []string{baseURLEstRus, baseURLEstUkr}
+		translationDirections = []string{"est-rus", "est-ukr"}
 	}
 
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	captureErrorIfNotNull(err)
 	var articles []string
+	for i := 0; i < len(urls); i++ {
+		url := urls[i]
+		translationDirection := translationDirections[i]
 
-	doc.Find(cartSelector).Each(func(i int, page *goquery.Selection) {
-		for i := 0; i < len(page.Nodes); i++ {
-			article, isMainArticle := GetSingleArticle(searchWord, page.Nodes[i])
-			if article == "" {
-				continue
-			}
-			if isMainArticle {
-				articles = append([]string{article}, articles...) //put main article to the first position
-				continue
-			}
-			articles = append(articles, article)
+		res, err := http.Get(fmt.Sprintf("%s%s", url, searchWord))
+		captureErrorIfNotNull(err)
+		defer res.Body.Close()
+
+		if res.StatusCode != 200 {
+			sentry.CaptureException(fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status))
 		}
-	})
+
+		// Load the HTML document
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		captureErrorIfNotNull(err)
+
+		doc.Find(cartSelector).Each(func(i int, page *goquery.Selection) {
+			for i := 0; i < len(page.Nodes); i++ {
+				article, isMainArticle := GetSingleArticleWithDirection(searchWord, page.Nodes[i], translationDirection)
+				if article == "" {
+					continue
+				}
+
+				if translationDirection == "est-ukr" {
+					// prepend ukrainian flag emoji
+					article = "🇺🇦 " + article
+				}
+				
+				if isMainArticle {
+					articles = append([]string{article}, articles...) //put main article to the first position
+					continue
+				}
+				articles = append(articles, article)
+			}
+		})
+	}
 
 	return articles
 }
